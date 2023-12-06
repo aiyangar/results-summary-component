864b4fbf54f3f1dd2be1b47a41b5a571bd306280
ing.gustavo.cardenas <ing.gustavo.cardenas@gmail.com>
2023-12-06 16:33:10 +0000

    finalizado

--- src/components/element.component.jsx
+++ src/components/element.component.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import data from '../data.json';
-import './element.styles.css'
+import './element.styles.css';
 
 const Element = () => {
   return (
     <div className='elementContainer'>
       {data.map((item, index) => (
-        <div key={index} className='singleElement'>
+        <div key={index} className='singleElement' style={{ backgroundColor: item.backgroundColor }}>
           <div className="imageAndCategory">
             <img src={item.icon} alt={item.category} />
-            <p>{item.category}</p>
+            <p style={{ color: item.color }}>{item.category}</p>
           </div>
-          
-          <p>{item.score}/100</p>
+          <div className="score">
+            <h4 className='scoreNumber'>{item.score}</h4>
+            <p>/100</p>
+          </div>
         </div>
       ))}
     </div>
