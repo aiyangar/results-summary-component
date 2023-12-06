a249f3afde1e28ec1c1718214e445cbdff958d3f
ing.gustavo.cardenas <ing.gustavo.cardenas@gmail.com>
2023-12-06 15:55:32 +0000

    se cambio el folder assets a public

--- src/components/element.component.jsx
+++ src/components/element.component.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import data from '../data.json';
 import './element.styles.css'
-import image from '../assets/images/icon-reaction.svg'
 
 const Element = () => {
   return (
     <div className='elementContainer'>
       {data.map((item, index) => (
         <div key={index} className='singleElement'>
-          <img src={item.icon} alt={item.category} />
-          {console.log(item.icon)}
-          <img src={image}/>
-          <p>{item.category}</p>
+          <div className="imageAndCategory">
+            <img src={item.icon} alt={item.category} />
+            <p>{item.category}</p>
+          </div>
+          
           <p>{item.score}/100</p>
         </div>
       ))}
